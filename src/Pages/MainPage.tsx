586a9e7b62fe2ac3a6546ffbe25853953d7c67bc
cosmin.oros <oroscosmin09@gmail.com>
2024-11-08 16:00:23 +0200

    main refactoring

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import '../styles.css'
+import '../styles.css';
 import { useNavigate } from 'react-router-dom';
 import { PageRoutes } from '../Routes/PageRoutes';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import Lottie from 'react-lottie';
-import trotineta from '../assets/trotineta.json'
+import trotineta from '../assets/trotineta.json';
 import Card from '../Components/Card';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAvN5FYavctE5fGBgkKdHUNjX--9lzACks",
-  authDomain: "trotecotm.firebaseapp.com",
-  projectId: "trotecotm",
-  storageBucket: "trotecotm.appspot.com",
-  messagingSenderId: "63406994923",
-  appId: "1:63406994923:web:b943024511d07f630095ae",
-  measurementId: "G-2WSVQ96EPW"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -43,20 +29,20 @@ const MainPage = () => {
   };
 
   return (
-    <div className='main-container'>
-      <div className='main-header'>
-        <h1>Trot Eco TM</h1>
+    <div className="main-container">
+      <div className="main-header">
+        <h1 className="title">Trot Eco TM</h1>
       </div>    
-      <div className='content'>
-        <div className='lottie-container'>
+      <div className="content">
+        <div className="lottie-container">
           <Lottie options={defaultOptions} height={400} width={400} />
         </div>
-        <div className='row-container'>
-          <button className='btn-main' onClick={onPostPress}>
-            Posteaza
+        <div className="row-container">
+          <button className="btn-main" onClick={onPostPress}>
+            Post
           </button>
-          <button className='btn-main' onClick={onSearchPress}>
-            Cauta
+          <button className="btn-main" onClick={onSearchPress}>
+            Search
           </button>
         </div>
       </div>
